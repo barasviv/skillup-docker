08710c99482dbd7be04f26ccc03c55d09607c455
nordea <baraskar.vivek@gmail.com>
2019-10-17 13:18:11 -0400

    FirstCommit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+ARG JAVA_VERSION=8u192-zulu-alpine
+
+FROM maven:3-jdk-8 as build
+
+WORKDIR /build
+COPY src src
+COPY pom.xml .
+RUN mvn clean install 
+
+
+
+FROM mcr.microsoft.com/java/jdk:${JAVA_VERSION} as runtime
+WORKDIR /app
+COPY --from=build /build/target/FirstMicroservice-1.0-SNAPSHOT.jar .
+
+RUN touch file1
+CMD java -jar FirstMicroservice-1.0-SNAPSHOT.jar
+
---
+++ src/main/java/com/nordea/microservices/controller/Api.java
@@ -0,0 +1,17 @@
+package com.nordea.microservices.controller;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/")
+public class Api {
+
+    @RequestMapping(method = RequestMethod.GET, value = "hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
